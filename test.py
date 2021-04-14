4344b852170040564bdf42813ac964045ba2c8b1
brenda <38980744@qq.com>
2021-04-14 14:44:11 +0800

    测试一下

---
+++ test.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+# Author:念奴娇
+# date:4/12/2021 2:20 PM
+# year:2021
+from selenium import webdriver
+def testcase():
+    driver=webdriver.Chrome()
+    driver.get('https://www.baidu.com')
